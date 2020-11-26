69a8a0779e43982e16255061a05676ca9ae750fd
Fabio Rodrigues <fabiordrig@gmail.com>
2020-11-26 14:25:07 -0300

    feat(bcrypt): ensure BcryptAdapter throws if bcrypt throws

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -28,4 +28,14 @@ describe('Bcrypt Adapter', () => {
 
     expect(hash).toBe('hash')
   })
+
+  test('Should throw if bcrypt throws', async () => {
+    const sut = makeSut()
+
+    jest.spyOn(bcrypt, 'hash').mockReturnValueOnce(new Promise((resolve,reject) => reject(new Error())))
+
+    const promise = sut.encrypt('anyValue')
+
+    await expect(promise).rejects.toThrow()
+  })
 })
