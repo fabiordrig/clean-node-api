c42de31cf74ab6dbfd44aa566ea91e463b2197ec
Fabio Rodrigues <fabiordrig@gmail.com>
2020-11-29 20:42:59 -0300

    feat(body-parser): ensure requests parse body as json

---
+++ src/main/middlewares/body-parser.test.ts
@@ -0,0 +1,13 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('Body Parser middleware', () => {
+  test('Should parse body as json', async () => {
+    app.post('/test-body-parser', (req, res) => { res.send(req.body) })
+
+    await request(app)
+      .post('/test-body-parser')
+      .send({ name: 'Fabio ' })
+      .expect({ name: 'Fabio ' })
+  })
+})
