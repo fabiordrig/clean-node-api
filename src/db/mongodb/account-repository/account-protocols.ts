831cefaccb03199810d2ac470673c08ba4925e8e
Fabio Rodrigues <fabiordrig@gmail.com>
2020-11-30 11:24:32 -0300

    feat(router): ensure SignUp route returns an account on success

--- src/db/mongodb/account-repository/account-protocols.ts
+++
@@ -1,3 +0,0 @@
-export * from '../../../data/protocols/add-account-repository'
-export * from '../../../domain/models/account'
-export * from '../../../domain/usecases/add-account'
---
+++ src/main/routes/signup-routes.test.ts
@@ -0,0 +1,29 @@
+import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import app from '../config/app'
+
+describe('SignUp route', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const account = MongoHelper.getCollection('accounts')
+    await account.deleteMany({})
+  })
+  test('Should an account on success', async () => {
+    await request(app)
+      .post('/api/signup')
+      .send({
+        name: 'Fabio',
+        email: 'fabio@gmail.com',
+        password: '1234',
+        passwordConfirmation: 1234
+      })
+      .expect(200)
+  })
+})
