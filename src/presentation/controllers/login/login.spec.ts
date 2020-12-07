1dee7db9bf10ea014796079a53eb5c607d5a10c8
Fabio Rodrigues <fabiordrig@gmail.com>
2020-12-07 18:56:48 -0300

    feat(login): ensure LoginController returns 500 if EmailValidator throws

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,6 +1,6 @@
 import { LoginController } from './login'
-import { badRequest } from '../../helper'
-import { InvalidParamError, MissingParamError } from '../../errors'
+import { badRequest, serverError } from '../../helper'
+import { InvalidParamError, MissingParamError, ServerError } from '../../errors'
 import { EmailValidator } from '../../protocols/email-validator'
 import { HttpRequest } from '../../protocols'
 
@@ -66,5 +66,17 @@ describe('Login Controller', () => {
     await sut.handle(makeFakeRequest())
 
     expect(isValidSpy).toHaveBeenCalledWith('anyMail@mail.com')
+  })
+
+  test('Should return 500 if EmailValidator throws', async () => {
+    const { sut, emailValidatorStub } = makeSut()
+
+    jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce(() => {
+      throw new Error()
+    })
+
+    const httpResponse = await sut.handle(makeFakeRequest())
+
+    expect(httpResponse).toEqual(serverError(new ServerError()))
   })
 })
--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,5 +1,5 @@
 import { InvalidParamError, MissingParamError } from '../../errors'
-import { badRequest } from '../../helper'
+import { badRequest, serverError } from '../../helper'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
 import { EmailValidator } from '../../protocols/email-validator'
 
@@ -12,17 +12,20 @@ export class LoginController implements Controller {
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const { email, password } = httpRequest.body
+    try {
+      if (!email) {
+        return new Promise((resolve) => resolve(badRequest(new MissingParamError('email'))))
+      }
+      if (!password) {
+        return new Promise((resolve) => resolve(badRequest(new MissingParamError('password'))))
+      }
 
-    if (!email) {
-      return new Promise((resolve) => resolve(badRequest(new MissingParamError('email'))))
-    }
-    if (!password) {
-      return new Promise((resolve) => resolve(badRequest(new MissingParamError('password'))))
-    }
-
-    const isValid = this.emailValidator.isValid(email)
-    if (!isValid) {
-      return new Promise((resolve) => resolve(badRequest(new InvalidParamError('email'))))
+      const isValid = this.emailValidator.isValid(email)
+      if (!isValid) {
+        return new Promise((resolve) => resolve(badRequest(new InvalidParamError('email'))))
+      }
+    } catch (err) {
+      return serverError(err)
     }
   }
 }
