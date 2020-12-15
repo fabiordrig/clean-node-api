af6b4592042f9c50ea8c89093eaad8c5cb28662b
Fabio Rodrigues <fabiordrig@gmail.com>
2020-12-15 19:50:24 -0300

    refactor: remove utils layer

--- src/main/factories/signup/signup-validation-factory.ts
+++ src/main/factories/signup/signup-validation-factory.ts
@@ -1,7 +1,7 @@
 
 import { ValidationComposite, CompareFieldValidation, RequiredFieldValidation, EmailValidation } from '../../../presentation/helper/validators'
 import { Validation } from '../../../presentation/protocols/validation'
-import { EmailValidatorAdapter } from '../../../utils/email-validator-adapter'
+import { EmailValidatorAdapter } from '../../adapter/validators/email-validator-adapter'
 
 export const makeSignUpValidation = (): ValidationComposite => {
   const validations: Validation[] = []
