e049a850d4b2f3797e9b2c3f5d02d08d75c6cf55
Fabio Rodrigues <fabiordrig@gmail.com>
2020-12-15 20:20:11 -0300

    test(login): login route returns 401 if invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -48,5 +48,14 @@ describe('Login route', () => {
         })
         .expect(200)
     })
+    test('Should return 401 on login fails', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'fabio@gmail.com',
+          password: '1234'
+        })
+        .expect(401)
+    })
   })
 })
