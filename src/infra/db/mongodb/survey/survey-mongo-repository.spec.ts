29d2741d0c870918baa5aa74f041df2d56932cfe
Fabio Rodrigues <fabiordrig@gmail.com>
2021-01-19 20:08:19 -0300

    feat(survey): ensure SurveyMongoRepository add a new survey

---
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -0,0 +1,43 @@
+import { Collection } from 'mongodb'
+import { MongoHelper } from '../helpers/mongo-helper'
+import { SurveyMongoRepository } from './survey-mongo-repository'
+
+let surveyCollection: Collection
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+  })
+
+  const makeSut = (): SurveyMongoRepository => {
+    return new SurveyMongoRepository()
+  }
+
+  test('Should add a survey on success', async () => {
+    const sut = makeSut()
+
+    await sut.add({
+      question: 'anyQuestion',
+      answers: [
+        {
+          image: 'anyImage',
+          answer: 'anyAnswer'
+        },
+        {
+          answer: 'anyAnswer'
+        }
+      ]
+    })
+
+    const survey = await surveyCollection.findOne({ question: 'anyQuestion' })
+    expect(survey).toBeTruthy()
+  })
+})
