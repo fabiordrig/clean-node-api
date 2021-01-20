8eae9fe12b2067f296dc102a7bdc6aaf739a7e86
Fabio Rodrigues <fabiordrig@gmail.com>
2021-01-20 18:37:44 -0300

    feat(survey): add AddSurvey composition

---
+++ src/main/routes/survey-routes.test.ts
@@ -0,0 +1,36 @@
+import { Collection } from 'mongodb'
+import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import app from '../config/app'
+
+let surveyCollection: Collection
+describe('Login route', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+  })
+  describe('POST /surveys', () => {
+    test('Should return 200 on signup success', async () => {
+      await request(app)
+        .post('/api/surveys')
+        .send({
+          question: 'anyQuestion',
+          answers: [
+            {
+              answer: 'anyAnswer',
+              image: 'http://image-name.com'
+            }
+          ]
+        })
+        .expect(204)
+    })
+  })
+})
