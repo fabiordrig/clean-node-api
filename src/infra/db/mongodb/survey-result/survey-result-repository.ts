7d5ad2d70846305bd002bb78e43bf9d66d35353f
Fabio Rodrigues <fabiordrig@gmail.com>
2021-02-14 23:10:53 -0300

    test: ensure SaveSurveyResultRepository returns null on load without answers

--- src/infra/db/mongodb/survey-result/survey-result-repository.ts
+++ src/infra/db/mongodb/survey-result/survey-result-repository.ts
@@ -191,6 +191,6 @@ implements SaveSurveyResultRepository, LoadSurveyResultRepository {
       .build()
 
     const surveyResult = await surveyResultCollection.aggregate(query).toArray()
-    return surveyResult?.length ? surveyResult[0] : null
+    return surveyResult.length ? surveyResult[0] : null
   }
 }
